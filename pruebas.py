6cc84a74b3b7c0a6e57ab73c7bfa8271bb1fbad0
LAB_F201 <labf201@continental.edu.pe>
2024-04-08 11:01:21 -0500

    sueldos

--- pruebas.py
+++ pruebas.py
@@ -17,8 +17,35 @@ def Derecha(message1, message2):
     tamaño = len(message1) + len(message2)
     a = round(50 - tamaño)
     print(message1, "."*a, message2)
+# cambio de saloess a solares
+    
+def SolaDolar(soles):
+    total = soles*3.8
+    return total 
 
-Derecha("Frugos", "S/. 15")
-Derecha("Oreo", "S/. 5")
+# print("Ingrese el monto a cambiar")
+# soles = float(input(":> "))
+# print("El dolares es: ", SolaDolar(soles))
 
-# Estoy en git
+def Sueldo(horas, monto):
+    bruto = horas * monto
+    afp = bruto*0.11
+    salud = bruto*0.05
+    neto = bruto - afp - salud
+
+    return(bruto, neto, afp, salud)
+
+name = input("Nombre del trbajador: ")
+
+
+print("Ingrese el sueldo de ", name)
+horas = float(input(":> "))
+print("Ingrese el monto por hora de ", name)
+monto = float(input(":> "))
+
+sueldos = Sueldo(horas, monto)
+print("El suelo bruto es: ", sueldos[0])
+print("El sueldo neto es: ", sueldos[1])
+print("Descuentos")
+print("AFP 11%: ",sueldos[2])
+print("EsSalud 5%: ",sueldos[3])
